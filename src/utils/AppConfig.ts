9e9dd2f8744c963305b89037d6a206153586ecc8
tomaszciula <tomciuns@gmail.com>
2022-11-18 18:06:07 +0100

    appconfig

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'FirstProject Group',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  title: 'Cataloger for your stuff',
+  description: 'Nextjs with Tailwind CSS',
   locale: 'en',
 };
